6cdf60d222857918bf952a0bcecc068a7432e29e
Salami Bashir <bashirsalami78@gmail.com>
2024-05-20 18:55:01 +0100

    chore: cleanup

--- src/Domain/Exceptions/UnsupportedColourException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ActivityManager.Domain.Exceptions;
-
-public class UnsupportedColourException : Exception
-{
-    public UnsupportedColourException(string code)
-        : base($"Colour \"{code}\" is unsupported.")
-    {
-    }
-}
